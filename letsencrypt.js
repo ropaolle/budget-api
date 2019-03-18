ae91fda86183895c32805123db693d1644ca4f49
ropaolle <ropaolle@gmail.com>
2019-03-18 15:31:02 +0100

    Remove unused file

--- letsencrypt.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.static(__dirname, { dotfiles: 'allow' }));
-
-app.listen(80, () => {
-  console.info('HTTP server running on port 80');
-});
